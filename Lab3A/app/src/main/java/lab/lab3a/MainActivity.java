d033641377ea36e9ea24dba08316d6e49d4296b9
Rami <rami.alsawadi1@gmail.com>
2015-12-04 20:22:02 +0100

    First

---
+++ Lab3A/app/src/main/java/lab/lab3a/MainActivity.java
@@ -0,0 +1,15 @@
+package lab.lab3a;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ Lab3A/app/src/main/java/lab/lab3a/View/FlowerView.java
@@ -0,0 +1,16 @@
+package lab.lab3a.View;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Rami on 2015-12-04.
+ */
+public class FlowerView extends View {
+    private boolean hasLeaves;
+
+    public FlowerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
