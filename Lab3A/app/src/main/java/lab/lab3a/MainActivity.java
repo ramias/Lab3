5333b354aee6f9b964fa51d99e4e8cdb8d20703d
Thomas <brandertb@gmail.com>
2015-12-06 17:14:58 +0100

    3A - Shake och vissnande blomma (ett håll bara)

--- Lab3A/app/src/main/java/lab/lab3a/MainActivity.java
+++ Lab3A/app/src/main/java/lab/lab3a/MainActivity.java
@@ -8,53 +8,47 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
 
     private Animation fallingLeafAnimation;
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
     private long lastUpdate = 0;
-    private float last_x, last_y, last_z;
-    private static final int SHAKE_THRESHOLD = 600;
-    private ImageView img[], shank[], pupil;
+    private float oldX, oldY, oldZ;
+    private static final int SHAKE_LIMIT = 500;
+    private ImageView leaf[], shank[], pupil, dryPupil, dryLeaf[];
+    private int lastShankPosition = 0;
+    private boolean isFlowerDead, hasFallen;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         fallingLeafAnimation = AnimationUtils.loadAnimation(this, R.anim.falling_leaf);
 
         pupil = (ImageView) findViewById(R.id.pupil);
+        dryPupil = (ImageView) findViewById(R.id.drypupil);
         shank = new ImageView[10];
-        img = new ImageView[5];
-
-        shank[0] =(ImageView) findViewById(R.id.shank1);
-        img[0] = (ImageView) findViewById(R.id.leaf1);
-        img[1] = (ImageView) findViewById(R.id.leaf2);
-        img[2] = (ImageView) findViewById(R.id.leaf3);
-        img[3] = (ImageView) findViewById(R.id.leaf4);
-        img[4] = (ImageView) findViewById(R.id.leaf5);
-
-        // positions
-        img[0].setX(80);
-        img[0].setY(70);
-        img[1].setX(-35);
-        img[1].setY(80);
-        img[2].setX(-70);
-        img[2].setY(100);
-        img[3].setX(70);
-        img[3].setY(25);
-        img[4].setX(25);
-        img[4].setY(20);
+        leaf = new ImageView[5];
+        dryLeaf = new ImageView[5];
+        isFlowerDead = false;
+        initializeImageViews();
 
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -71,6 +65,27 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, Menu.NONE, "New Flower!");
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == 0) {
+            recreate(); // Kan vara en bugg här, outOfMemory Exception
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
@@ -79,29 +94,107 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             float x = sensorEvent.values[0];
             float y = sensorEvent.values[1];
             float z = sensorEvent.values[2];
+            long currentTime = System.currentTimeMillis();
 
-            long curTime = System.currentTimeMillis();
+            if ((currentTime - lastUpdate) > 150) { // Gränsvärde för hur ofta en förändring ska ta effekt. Nu var 150 ms
+                long deltaTime = (currentTime - lastUpdate);
+                lastUpdate = currentTime;
 
-            if ((curTime - lastUpdate) > 100) {
-                long diffTime = (curTime - lastUpdate);
-                lastUpdate = curTime;
+                float velocity = Math.abs(x + y + z - oldX - oldY - oldZ) / deltaTime * 10000;
 
-                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
-
-                if (speed > SHAKE_THRESHOLD) {
-                    for(ImageView i:img) i.startAnimation(fallingLeafAnimation);
+                // Skakar bort löven
+                if (velocity > SHAKE_LIMIT && !isFlowerDead && !hasFallen) {
+                    hasFallen = true;
+                    for (ImageView i : leaf) i.startAnimation(fallingLeafAnimation);
+                } else if (velocity > SHAKE_LIMIT && isFlowerDead && !hasFallen) {
+                    hasFallen = true;
+                    for (ImageView i : dryLeaf) i.startAnimation(fallingLeafAnimation);
+                } else {
+                    int newShankPosition = (int) Math.abs(Math.round(Math.ceil(x)));
+                    if (lastShankPosition != newShankPosition && newShankPosition < 10) {
+                        shank[lastShankPosition].setVisibility(View.INVISIBLE);
+                        if (x < -9 && x >= -10) {
+                            isFlowerDead = true;
+                            shank[9].setVisibility(View.VISIBLE);
+                            dryPupil.setVisibility(View.VISIBLE);
+                            pupil.setVisibility(View.INVISIBLE);
+                            if (!hasFallen) {
+                                for (ImageView i : leaf)  i.setVisibility(View.INVISIBLE);
+                                for (ImageView i : dryLeaf) i.setVisibility(View.VISIBLE);
+                            }
+                            lastShankPosition = newShankPosition;
+                            return;
+                        } else {
+                            isFlowerDead = false;
+                            shank[9].setVisibility(View.INVISIBLE);
+                            dryPupil.setVisibility(View.INVISIBLE);
+                            pupil.setVisibility(View.VISIBLE);
+                            if (!hasFallen) {
+                                for (ImageView i : leaf) i.setVisibility(View.VISIBLE);
+                                for (ImageView i : dryLeaf) i.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                        shank[newShankPosition].setVisibility(View.VISIBLE);
+                        lastShankPosition = newShankPosition;
+                    }
                 }
-
-                last_x = x;
-                last_y = y;
-                last_z = z;
+                oldX = x;
+                oldY = y;
+                oldZ = z;
             }
-            Log.i("aa","X: "+x);
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    private void initializeImageViews() {
+        shank[0] = (ImageView) findViewById(R.id.shank1);
+        shank[1] = (ImageView) findViewById(R.id.shank2);
+        shank[2] = (ImageView) findViewById(R.id.shank3);
+        shank[3] = (ImageView) findViewById(R.id.shank4);
+        shank[4] = (ImageView) findViewById(R.id.shank5);
+        shank[5] = (ImageView) findViewById(R.id.shank6);
+        shank[6] = (ImageView) findViewById(R.id.shank7);
+        shank[7] = (ImageView) findViewById(R.id.shank8);
+        shank[8] = (ImageView) findViewById(R.id.shank9);
+        shank[9] = (ImageView) findViewById(R.id.dryshank);
+
+        leaf[0] = (ImageView) findViewById(R.id.leaf1);
+        leaf[1] = (ImageView) findViewById(R.id.leaf2);
+        leaf[2] = (ImageView) findViewById(R.id.leaf3);
+        leaf[3] = (ImageView) findViewById(R.id.leaf4);
+        leaf[4] = (ImageView) findViewById(R.id.leaf5);
+
+        dryLeaf[0] = (ImageView) findViewById(R.id.dryleaf1);
+        dryLeaf[1] = (ImageView) findViewById(R.id.dryleaf2);
+        dryLeaf[2] = (ImageView) findViewById(R.id.dryleaf3);
+        dryLeaf[3] = (ImageView) findViewById(R.id.dryleaf4);
+        dryLeaf[4] = (ImageView) findViewById(R.id.dryleaf5);
+
+        // positions
+        dryLeaf[0].setX(80);
+        dryLeaf[0].setY(70);
+        dryLeaf[1].setX(-35);
+        dryLeaf[1].setY(80);
+        dryLeaf[2].setX(-70);
+        dryLeaf[2].setY(100);
+        dryLeaf[3].setX(70);
+        dryLeaf[3].setY(25);
+        dryLeaf[4].setX(25);
+        dryLeaf[4].setY(20);
+
+        leaf[0].setX(80);
+        leaf[0].setY(70);
+        leaf[1].setX(-35);
+        leaf[1].setY(80);
+        leaf[2].setX(-70);
+        leaf[2].setY(100);
+        leaf[3].setX(70);
+        leaf[3].setY(25);
+        leaf[4].setX(25);
+        leaf[4].setY(20);
     }
 }
